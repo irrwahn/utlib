d4411bffdb7e7a79619a50512fdc5e6935350fd0
irrwahn <irrwahn35@freenet.de>
2017-12-14 15:30:12 +0100

    * Added test module for str_move, str_trim, str_skip.

---
+++ test/str_trim.c
@@ -0,0 +1,134 @@
+#include <string.h>
+
+#include <str_trim.h>
+
+#include "testsupp.h"
+
+
+int REGISTER( str_move_test )( int id )
+{
+    int i, err = 0;
+    char s[100];
+    const char *o  = "Hello World";
+    const char *r[] = {
+        "World",
+        "Hello Hello World",
+    };
+
+    i = 0;
+    strcpy( s, o );
+    if ( strcmp( str_move( s, s + 6 ), r[i] ) )
+    {
+        ++err;
+        FAIL( id, "str_move failed on index %d", i );
+    }
+    ++i;
+
+    strcpy( s, o );
+    if ( strcmp( str_move( s + 6, s ), o ) || strcmp( s, r[i] ) )
+    {
+        ++err;
+        FAIL( id, "str_move failed on index %d", i );
+    }
+    ++i;
+
+    if ( !err )
+        PASS( id, "str_move_test %d/%d", i, i );
+    return err;
+}
+
+
+int REGISTER( str_trim_test )( int id )
+{
+    int i, err = 0;
+    char buf[400];
+    static const struct {
+        const char *o;
+        const char *tr;
+        const char *ltr;
+        const char *rtr;
+    } str_ttest[] = {
+        /*    orig    trim   ltrim   rtrim */
+        {       "",     "",     "",     "" },
+        {      " ",     "",     "",     "" },
+        {     "  ",     "",     "",     "" },
+        {      "x",    "x",    "x",    "x" },
+        {     " x",    "x",    "x",   " x" },
+        {     "x ",    "x",   "x ",    "x" },
+        {    " x ",    "x",   "x ",   " x" },
+        {  " x\t ",    "x", "x\t ",   " x" },
+        {  " x y ",  "x y", "x y ", " x y" },
+        {     NULL,   NULL,   NULL,   NULL }
+    };
+
+    for ( i = 0; str_ttest[i].o; ++i )
+    {
+        strcpy( buf, str_ttest[i].o );
+        if ( strcmp( str_trim(buf), str_ttest[i].tr ) )
+        {
+            ++err;
+            FAIL( id, "str_trim failed on index %d", i );
+        }
+        strcpy( buf, str_ttest[i].o );
+        if ( strcmp( str_ltrim(buf), str_ttest[i].ltr ) )
+        {
+            ++err;
+            FAIL( id, "str_ltrim failed on index %d", i );
+        }
+        strcpy( buf, str_ttest[i].o );
+        if ( strcmp( str_rtrim(buf), str_ttest[i].rtr ) )
+        {
+            ++err;
+            FAIL( id, "strrtrim failed on index %d", i );
+        }
+    }
+    if ( !err )
+        PASS( id, "str_trim_test %d/%d", i, i );
+    return 0;
+}
+
+
+int REGISTER( str_skip_test )( int id )
+{
+    int i, err = 0;
+    static const struct {
+        const char *o;
+        const char *set;
+        const char *sk;
+        const char *sksp;
+    } str_stest[] = {
+        /*        orig      skipset          skip    skipspace */
+        {           "",         "",           "",           "" },
+        {          " ",         "",          " ",           "" },
+        {          " ",        " ",           "",           "" },
+        {  " \r\n\v\t",        " ",   "\r\n\v\t",           "" },
+        {          "Y",        " ",          "Y",          "Y" },
+        {         " Y",        " ",          "Y",          "Y" },
+        {        " xY",        "x",        " xY",         "xY" },
+        {        " xY",       "x ",          "Y",         "xY" },
+        {        " xY",       " x",          "Y",         "xY" },
+        {        " xY",      "Yx ",           "",         "xY" },
+        {     "abc Y ",     "abc ",         "Y ",     "abc Y " },
+        {       " abc",     " aBC",         "bc",        "abc" },
+        {         NULL,       NULL,         NULL,         NULL }
+    };
+
+    for ( i = 0; str_stest[i].o; ++i )
+    {
+        if ( strcmp( str_skip( str_stest[i].o, str_stest[i].set ), str_stest[i].sk ) )
+        {
+            ++err;
+            FAIL( id, "str_skip failed on index %d", i );
+        }
+        if ( strcmp( str_skipspace( str_stest[i].o ), str_stest[i].sksp ) )
+        {
+            ++err;
+            FAIL( id, "str_skipspace failed on index %d", i );
+        }
+    }
+    if ( !err )
+        PASS( id, "str_skip_test %d/%d", i, i );
+    return 0;
+}
+
+/* EOF */
