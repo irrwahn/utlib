5a619626a2240b2e3758f35bdc2e2165650eacdd
Urban Wallasch <info@latice.de>
2017-12-21 10:18:40 +0100

    * Reconstructed a merge gone wrong (added logging_test.c module; fixed macro in testsupp.h; updated TODO). * Updated dist.lst

---
+++ test/logging_test.c
@@ -0,0 +1,52 @@
+#include <errno.h>
+#include <string.h>
+
+#include <logging.h>
+
+#include "testsupp.h"
+
+/*
+ * If someone can come up with a way to test syslog logging, without
+ * actually messing up the system log and yet still be able to check
+ * for the message to hit the log: Please let me know!
+ */
+
+REGISTER(logging_test)
+{
+    int err = 0;
+    FILE *fp;
+    char buf[500];
+
+    log_printf( LOG_DEBUG, "log_%s %m\n", "default" );
+
+    while ( NULL == ( fp = tmpfile() ) )
+        ;
+    errno = 0;
+    log_close();
+    log_open( LOG_DEBUG, LOG_TO_FILE, fp, "log_to_file", 0, 0 );
+    log_xprintf( LOG_INFO, "Test: %m\n" );
+    rewind( fp );
+    if ( NULL != fgets( buf, sizeof buf, fp ) )
+    {
+        fprintf(stderr,"%s", buf);
+        if ( !strstr( buf, "Test: Success" )
+            || !strstr( buf, ": (" __FILE__ ":logging_test:" ) )
+            ++err;
+    }
+    else
+        ++err;
+
+#line 775 "fake.file"
+    log_close();
+    log_open( LOG_DEBUG, LOG_TO_FILE, stderr, "log_to_stderr", 0, 0 );
+    log_xprintf( LOG_DEBUG, "%m\n" );
+#line 44 "logging_test.c"
+
+    if ( !err )
+        PASS( "logging ok" );
+    else
+        FAIL( "logging failed, %d errors", err );
+    return err;
+}
+
+/* EOF */
